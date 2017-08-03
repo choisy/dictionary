09262d98bf7abc61ac40d2bbad3d907ce7fb57fd
Marc Choisy <mchoisy@gmail.com>
2017-08-03 16:32:31 +0700

    Adding the description of the provinces per year

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Dictionary of Vietnamese provinces names
 
 The dictionary is a named vector in dictionary::provinces that can be used for hashing.
+
+The description of the provinces per time period is in dictionary::province_year
--- data-raw/make_data.R
+++ data-raw/make_data.R
@@ -1,4 +1,9 @@
+# Dictionary of the names of the provinces -------------------------------------
+
 provinces <- read.table("data-raw/provinces.txt", sep = ";",
                         header = TRUE, stringsAsFactors = FALSE)
 provinces <- with(provinces, setNames(new, old))
+
+# Writing to disk --------------------------------------------------------------
+
 devtools::use_data(provinces, overwrite = TRUE)
