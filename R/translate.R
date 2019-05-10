c9afc9f701e35b27d9be31de986f58cbe7f8d34c
Lucie Contamin <contamin.lucie@gmail.com>
2019-05-10 09:26:24 +0700

    Fix translate and add tests

--- R/translate.R
+++ R/translate.R
@@ -3,7 +3,14 @@
 #' Translates string vector in English without accent using dictionary created
 #' and stored in the package by country and level of administrative boundaries.
 #'
+#' The functions accepts two types of entry for translation. \cr
+#' 1. You can use the \code{country} and \code{level} information to translate
+#' vector, but only for Vietnam, Cambodia and Thailand.
+#' 2 . You can directly input a named vector in UNICODE containing the
+#' translation.
+#'
 #' @param vect a string chracter vector.
+#' @param hash a named UNICODE vector containing the translation
 #' @param country a character name of a country.
 #' @param level a numeric, 1 for admin1 (province), 2 for admin2 (district),
 #' 3 for city/commune.
@@ -16,18 +23,32 @@
 #'
 #' @examples
 #' # to translate province name of Vietnam in English
-#' translate(c("AnGiang", "Ha Noi"), "Vietnam", 1)
-translate <- function(vect, country, level){
+#' translate(c("AnGiang", "Ha Noi"), vn_province)
+#' # or
+#' translate(c("AnGiang", "Ha Noi"), country = "Vietnam", level = 1)
+translate <- function(vect, hash, country = NULL, level = NULL) {
+
+  if (missing(hash) & (is.null(country) | is.null(level))) {
+    stop(
+      "The argument 'hash' or the arguments 'country' & 'level'
+      should be inputed")
+  }
+  if (!is.null(country) & !is.null(level)) {
+    # get correct dictionary
+    country <- countrycode::countrycode(country, "country.name", "iso2c")
+    country <- tolower(country)
+    if (!country %in% c("vn", "th", "la", "vn")) {
+      stop("The arguments 'country' & 'level' can only be used for
+           Cambodia, Laos, Thailand and Vietnam")
+    }
+    # extract level
+    nlev <- c("province" = 1, "district" = 2, "commune" = 3)
+    level <- names(nlev)[level]
+    hash <- get(paste0(country, "_", level))
+  }
   # translates vect in UNICODE
   vect <-  stringi::stri_escape_unicode(vect)
-  # get correct dictionary
-  country <- countrycode::countrycode(country, "country.name", "iso2c")
-  country <- tolower(country)
-   # extract level
-  nlev <- c("province" = 1, "district" = 2, "commune" = 3)
-  level <- names(nlev)[level]
-  dict <- get(paste0(country, "_", level))
-  # translates
-  vect <- dict[vect]
+  vect <- hash[vect]
   vect
+
 }
