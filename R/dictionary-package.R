98c636c1e9db7eaeb88da66ddec01a48b9eafedd
Lucie Contamin <contamin.lucie@gmail.com>
2019-08-02 18:19:32 +0700

    export function dictionary

---
+++ R/dictionary-package.R
@@ -0,0 +1,9 @@
+#' dictionary: Dictionary of Vietnamese admin1 Names
+#'
+#' This package contains a dictionary for Vietnamese admin1 names, admin2
+#' names and admin3 names, useful to ensure consistency between different data
+#'  packages using vietnamese admin1, districts and/or admin3.
+#'
+#' @docType package
+#' @name dictionary
+NULL
